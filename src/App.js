a65debfa6496dee0c9e78b50507931af45ed03f2
Jhansi0307 <Jhansi.Batareddy@agilisium.com>
2022-03-12 23:28:02 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./Components/Dashboard";
+import Home from "./Components/Home";
+import Monthly from "./Components/Monthly";
+import Weekly from "./Components/Weekly";
+import Yearly from "./Components/Yearly";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/weekly' element={<Weekly/>}/>
+        <Route path='/monthly' element={<Monthly/>}/>
+        <Route path='/yearly' element={<Yearly/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+       
+      </Routes>
+    </>
+  );
+}
+
+export default App;
